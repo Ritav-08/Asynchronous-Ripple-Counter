008fd642fd34d3a1c4fecfdca966bd9416212976
Ritav <ritav08092004@gmail.com>
2026-04-27 19:00:15 +0530

    Update Design.v

--- Design.v
+++ Design.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 23.04.2026 19:56:27
-// Design Name: 
-// Module Name: aCounter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module aCounter(
    input mode_i,  //0 (UP), 1 (DOWN)
