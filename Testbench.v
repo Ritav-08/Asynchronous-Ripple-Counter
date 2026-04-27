f18e7ace0ae4d4851ab2021bc584d29e5a2bbc25
Ritav <ritav08092004@gmail.com>
2026-04-27 19:00:43 +0530

    Update Testbench.v

--- Testbench.v
+++ Testbench.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 23.04.2026 19:56:59
-// Design Name: 
-// Module Name: tb_aCounter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module tb_aCounter();
 reg mode_ti;
